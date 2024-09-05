e491087f090961b0e6502fb0cce28e493563c6e3
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-09-05 23:03:06 +0900

    prisma api Routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import Map from "@/components/Map";
 import Markers from "@/components/Markers";
 
-import * as stores from "@/data/store_data.json";
 import StoreBox from "@/components/StoreBox";
 import { StoreType } from "@/interface";
+
 export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
@@ -13,21 +13,19 @@ export default function Home({ stores }: { stores: StoreType[] }) {
   return (
     <>
       <Map setMap={setMap} />
-      <Markers
-        stores={stores}
-        map={map}
-        setCurrentStore={setCurrentStore}
-      />
+      <Markers stores={stores} map={map} setCurrentStore={setCurrentStore} />
       <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
 }
 
-export async function getStaticProps(){
-  const stores = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`).then((res) =>res.json());
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
 
-  return{
-    props:{stores},
-    revalidate:60*60,
-  }
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
